10156cd8b602f1b658479ca301744da66746a605
vineetsingh0809 <75788774+vineetsingh0809@users.noreply.github.com>
2021-10-18 22:25:27 +0530

    Practice problem of CodeChef

---
+++ pracprob29.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    do
+    {
+        cin >> a;
+        if (a != 42 && a < 100 && a > (-100))
+        {
+            cout << a << "\n";
+        }
+
+    } while (a != 42 && a < 100 && a > (-100));
+}
